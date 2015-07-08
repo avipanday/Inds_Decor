d901e1512e306c9d93e761153cf36cdf7ebdeda3
avipanday <avi.panday@hotmail.com>
2015-07-08 14:49:43 -0400

    Got the Insta Badge to Adjust
    Had to adjust javascript file so that the insta badge automatically
    moved up and down with the dropdown menu for mobile devices.

--- js/inds.js
+++ js/inds.js
@@ -4,12 +4,12 @@ function adjustCarousel() {
 	if (navbarStatus == 0) {
 		$("#c-left-control").css({ top: '267px'});
 		$("#c-right-control").css({ top: '267px'});
-		/*$("#ig-container").css({ padding-top: '217px'});*/
+		$("#ig-container").css({ "padding-top" : '217px'});
 		navbarStatus++;
 	} else {
 		$("#c-left-control").css({ top: '50px'});
 		$("#c-right-control").css({ top: '50px'});
-		/*$("#ig-container").css({ padding-top: '0px'});*/
+		$("#ig-container").css({ "padding-top" : '0px'});
 		navbarStatus--;
 	}
 }
